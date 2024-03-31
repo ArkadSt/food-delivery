d148d2749b0ba8a54eb09f2aac2a28b4988f8a08
Arkadi Statsenko <arkadistatsenko@gmail.com>
2024-03-31 23:14:52 +0300

    deleted unused setters

--- src/main/java/org/arkadst/fooddelivery/model/Station.java
+++ src/main/java/org/arkadst/fooddelivery/model/Station.java
@@ -36,44 +36,18 @@ public class Station {
 
     }
 
-    public String getStationName() {
-        return stationName;
-    }
-
-    public void setStationName(String stationName) {
-        this.stationName = stationName;
-    }
-
     public Double getAirTemp() {
         return airTemp;
-    }
-
-    public void setAirTemp(Double airTemp) {
-        this.airTemp = airTemp;
     }
 
     public Double getWindSpeed() {
         return windSpeed;
     }
 
-    public void setWindSpeed(Double windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
     public String getWeatherPhenomenon() {
         return weatherPhenomenon;
     }
 
-    public void setWeatherPhenomenon(String weatherPhenomenon) {
-        this.weatherPhenomenon = weatherPhenomenon;
-    }
-
-    public StationId getStationId() {
-        return stationId;
-    }
-
-    public void setStationId(StationId stationId) {
-        this.stationId = stationId;
-    }
 }
+
 
